561ad4fafe6e3f8a558030ed0eb4accba06440e7
jui1 <juimandal31@gmail.com>
2025-10-31 20:43:23 +0530

    maxprofit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4}; // sample input
+        int[] prices = {7, 1, 5, 3, 6, 4}; 
 
         Solution sol = new Solution();
         int maxProfit = sol.maxProfit(prices);
@@ -15,13 +15,13 @@ class Solution {
     public int maxProfit(int[] p) {
         if (p == null || p.length == 0) return 0;
 
-        int max = 0;         // maximum profit so far
-        int min = p[0];      // minimum buy price so far
+        int max = 0;         
+        int min = p[0];      
 
         for (int i = 1; i < p.length; i++) {
-            int profitToday = p[i] - min;       // profit if sell today
-            max = Math.max(max, profitToday);   // update max profit
-            min = Math.min(min, p[i]);          // update min buy price
+            int profitToday = p[i] - min;       
+            max = Math.max(max, profitToday);   
+            min = Math.min(min, p[i]);         
         }
 
         return max;
